733535a8142c94ad5941fb26ab42ed280fd9f2a3
rohitjogdand <113465978+rohitjogdand@users.noreply.github.com>
2024-06-01 08:52:09 +0530

    Add files via upload

---
+++ js/reg.js
@@ -0,0 +1,14 @@
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+  }
+  
+  function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+  }
+  function openForm1() {
+    document.getElementById("myForm").style.display = "block";
+  }
+  
+  function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+  }
